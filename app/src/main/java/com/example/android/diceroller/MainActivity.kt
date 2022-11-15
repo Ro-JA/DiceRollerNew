d8afc0507bb9ad9d563fc5814c57ccaf561f43a1
Roman <74274722+Ro-JA@users.noreply.github.com>
2022-11-16 00:40:02 +0900

    add Two object dice

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -6,25 +6,46 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button: Button = findViewById(R.id.button);
-        button.setOnClickListener {
-            rollDice()
-
-        }
+        button.setOnClickListener { rollDice() }
     }
 
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+
     private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it.
         val dice = Dice(6);
-        val diceRoll = dice.roll()
+        val diceRoll = dice.roll();
+        val diceTwo = Dice(20);
+        val diceRollTwo = diceTwo.roll();
+
+        //Update the screen with the dice roll.
         val resultTextView: TextView = findViewById(R.id.textView);
         resultTextView.text = diceRoll.toString();
+
+        val resultTextViewTwo: TextView = findViewById(R.id.textViewTwo);
+        resultTextViewTwo.text = diceRollTwo.toString();
     }
 }
 
+/**
+ * Dice with a fixed number of sides.
+ */
+
 class Dice(private val numSides: Int) {
+
+    /**
+     * Do a random dice roll and return the result.
+     */
     fun roll() = (1..numSides).random();
 }
