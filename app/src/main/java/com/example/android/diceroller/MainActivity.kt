16cafb63ae1592b3881a71e1d1e655d821dd93b8
Roman <74274722+Ro-JA@users.noreply.github.com>
2022-11-15 10:45:31 +0900

    add Toast

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.android.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button: Button = findViewById(R.id.button);
+        button.setOnClickListener {
+            Toast.makeText(this,"Dice Rolled!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
