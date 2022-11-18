7fbe87c3e525a220b592019c7b8cc64cc29b4780
Roman <74274722+Ro-JA@users.noreply.github.com>
2022-11-18 12:23:36 +0900

    add two Dice

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -25,9 +25,13 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
 //        Create new Dice object with 6 sides and roll it.
         val dice = Dice(6)
+        val diceTwo = Dice(6)
         val rollDice = dice.roll()
+        val rollDiceTwo = diceTwo.roll()
+
 //        Find the ImageView in the layout
         val diceImage: ImageView = findViewById(R.id.image_view)
+        val diceImageTwo: ImageView = findViewById(R.id.image_view_two)
 //        Determine which drawable resource ID to use based on the dice roll(присваеваем пременой drawableResource картинку из ресурсов в зависимости от значения пременой rollDice)
         val drawableResource = when (rollDice) {
             1 -> R.drawable.dice_1
@@ -37,10 +41,21 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
+
+        val drawableResourceTwo = when (rollDiceTwo) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 //        Update the ImageView with the correct drawable resource ID(обновляем картинку кости согласно значения пердоваемое из when)
         diceImage.setImageResource(drawableResource)
+        diceImageTwo.setImageResource(drawableResourceTwo)
 //        Update the content description(добовляем описания картинки через метод contentDescription пердовая значения в виде строки для спецальных возможностей андройда)
         diceImage.contentDescription = rollDice.toString()
+        diceImageTwo.contentDescription = rollDiceTwo.toString()
     }
 }
 
