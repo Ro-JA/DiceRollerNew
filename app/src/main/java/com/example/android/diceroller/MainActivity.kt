d7b788ff1b891c24ecf0ae4ec3df91432690f1c7
Roman <74274722+Ro-JA@users.noreply.github.com>
2022-11-18 11:04:30 +0900

    add contentDescription

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 
 /**
  * This activity allows the user to roll a dice and view the result
@@ -15,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button: Button = findViewById(R.id.button);
+        val button: Button = findViewById(R.id.button)
         button.setOnClickListener { rollDice() }
     }
 
@@ -25,18 +23,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         // Create new Dice object with 6 sides and roll it.
-        val dice = Dice(6);
-        val rollDice = dice.roll();
-        val diceImage: ImageView = findViewById(R.id.image_view);
+        val dice = Dice(6)
+        val rollDice = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.image_view)
         val drawableResource = when (rollDice) {
-            1 -> R.drawable.dice_1;
-            2 -> R.drawable.dice_2;
-            3 -> R.drawable.dice_3;
-            4 -> R.drawable.dice_4;
-            5 -> R.drawable.dice_5;
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(drawableResource);
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = rollDice.toString()
     }
 }
 
@@ -49,5 +48,5 @@ class Dice(private val numSides: Int) {
     /**
      * Do a random dice roll and return the result.
      */
-    fun roll() = (1..numSides).random();
+    fun roll() = (1..numSides).random()
 }
